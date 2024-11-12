fe5aef0231e31d0146503034c1ec03ab39c7151c
GG_Host <gd.host@yandex.ru>
2024-11-12 22:43:16 +0300

    feat: scripts updated

--- src/main/resources/db/company.sql
+++ src/main/resources/db/company.sql
@@ -1,3 +1,18 @@
+CREATE TYPE STATUS AS ENUM (
+    'ACTIVE',
+    'INACTIVE'
+);
+
 CREATE TABLE company(
-    id          UUID PRIMARY KEY DEFAULT uuid_generate_v4()
-)
+    id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name        VARCHAR(1024) NOT NULL,
+    admin_id    UUID NOT NULL,
+    contact_emails EMAIL[] NOT NULL,
+    contact_phones PHONE[] NOT NULL,
+    bil_address VARCHAR(128) NOT NULL UNIQUE,
+    tax_id VARCHAR(128) NOT NULL UNIQUE,
+    addresses VARCHAR(1024)[] NOT NULL,
+    status STATUS NOT NULL,
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at  TIMESTAMPTZ
+);
--- src/main/resources/db/user.sql
+++ src/main/resources/db/user.sql
@@ -1,13 +1,22 @@
 CREATE TABLE company_user(
     id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    company_id  UUID NOT NULL REFERENCES company(id),
     name        USER_NAME NOT NULL,
     password    VARCHAR(2048) NOT NULL,
     privileges  user_privilege[],
     email       EMAIL NOT NULL UNIQUE,
     phone       PHONE NOT NULL,
-    warehouses  UUID[],
     created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at  TIMESTAMPTZ
 );
+
+CREATE TABLE company_user_connection(
+    userId      UUID NOT NULL REFERENCES company_user(id),
+    companyId   UUID NOT NULL REFERENCES company(id)
+);
+
+CREATE TABLE company_user_warehouse(
+    userId      UUID NOT NULL REFERENCES company_user(id),
+    warehouse   UUID NOT NULL
+);
+
 
