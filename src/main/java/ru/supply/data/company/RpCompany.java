1022bedc556e89eae6d72c3959e33009fadfb970
GG_Host <gd.host@yandex.ru>
2024-11-13 01:28:23 +0300

    feat: RpCompany and RpUser updated

--- src/main/java/ru/supply/data/company/RpCompany.java
+++ src/main/java/ru/supply/data/company/RpCompany.java
@@ -15,6 +15,7 @@ import ru.supply.data.utils.user.UserPermission;
 import javax.sql.DataSource;
 import java.sql.Array;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -76,39 +77,58 @@ public class RpCompany {
                 .set(adminId)
                 .select((rset, stmt) -> {
                     if (rset.next()) {
-
-                        String emails = rset.getString("contact_emails");
-                        emails = emails.substring(1, emails.length() - 1);
-                        List<Email> companyEmails = Arrays.stream(
-                                emails.split(",")
-                        ).filter(string -> !string.isEmpty()).map(Email::new).toList();
-
-                        String phones = rset.getString("contact_phones");
-                        phones = phones.substring(1, phones.length() - 1);
-                        List<Phone> companyPhones = Arrays.stream(
-                                phones.split(",")
-                        ).filter(string -> !string.isEmpty()).map(Phone::new).toList();
-
-                        String addresses = rset.getString("addresses");
-                        addresses = addresses.substring(1, addresses.length() - 1);
-                        List<Address> companyAddresses = Arrays.stream(
-                                addresses.split(",")
-                        ).filter(string -> !string.isEmpty()).map(Address::new).toList();
-
-                        return Optional.of(new Company(
-                                UUID.fromString(rset.getString("id")),
-                                rset.getString("name"),
-                                UUID.fromString(rset.getString("admin_id")),
-                                companyEmails,
-                                companyPhones,
-                                new Bil(rset.getString("bil_address")),
-                                new Tax(rset.getString("tax_id")),
-                                companyAddresses,
-                                CompanyStatus.valueOf(rset.getString("status"))
-                        ));
+                        return compactCompanyFromResultSet(rset);
                     }
                     return Optional.empty();
                 });
+    }
+
+    public Optional<Company> getByName(String name) throws SQLException {
+        JdbcSession jdbcSession = new JdbcSession(dataSource);
+        return jdbcSession
+                .sql("""
+                        SELECT * FROM company
+                        WHERE name = ?
+                        """)
+                .set(name)
+                .select((rset, stmt) -> {
+                    if (rset.next()) {
+                        return compactCompanyFromResultSet(rset);
+                    }
+                    return Optional.empty();
+                });
+    }
+
+    private static Optional<Company> compactCompanyFromResultSet(ResultSet rset) throws SQLException {
+        String emails = rset.getString("contact_emails");
+        emails = emails.substring(1, emails.length() - 1);
+        List<Email> companyEmails = Arrays.stream(
+                emails.split(",")
+        ).filter(string -> !string.isEmpty()).map(Email::new).toList();
+
+        String phones = rset.getString("contact_phones");
+        phones = phones.substring(1, phones.length() - 1);
+        List<Phone> companyPhones = Arrays.stream(
+                phones.split(",")
+        ).filter(string -> !string.isEmpty()).map(Phone::new).toList();
+
+        String addresses = rset.getString("addresses");
+        addresses = addresses.substring(1, addresses.length() - 1);
+        List<Address> companyAddresses = Arrays.stream(
+                addresses.split(",")
+        ).filter(string -> !string.isEmpty()).map(Address::new).toList();
+
+        return Optional.of(new Company(
+                UUID.fromString(rset.getString("id")),
+                rset.getString("name"),
+                UUID.fromString(rset.getString("admin_id")),
+                companyEmails,
+                companyPhones,
+                new Bil(rset.getString("bil_address")),
+                new Tax(rset.getString("tax_id")),
+                companyAddresses,
+                CompanyStatus.valueOf(rset.getString("status"))
+        ));
     }
 
 }
--- src/main/java/ru/supply/data/user/RpUser.java
+++ src/main/java/ru/supply/data/user/RpUser.java
@@ -3,6 +3,7 @@ package ru.supply.data.user;
 import com.jcabi.jdbc.JdbcSession;
 import com.jcabi.jdbc.SingleOutcome;
 import lombok.AllArgsConstructor;
+import ru.supply.data.company.Company;
 import ru.supply.data.utils.Email;
 import ru.supply.data.utils.user.UserName;
 import ru.supply.data.utils.Phone;
@@ -12,6 +13,7 @@ import ru.supply.requestEntity.user.UserRequestEntity;
 import javax.sql.DataSource;
 import java.sql.Array;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.*;
@@ -20,108 +22,6 @@ import java.util.*;
 public class RpUser {
 
     public final DataSource dataSource;
-
-    public Optional<User> getByEmail(String email) throws SQLException {
-        JdbcSession jdbcSession = new JdbcSession(dataSource);
-        return jdbcSession
-                .sql("""
-                    SELECT u.id,
-                           (u.name).first_name as firstName,
-                           (u.name).second_name as secondName,
-                           (u.name).last_name as lastName,
-                           u.password,
-                           u.privileges,
-                           u.email,
-                           u.phone,
-                           u.created_at,
-                           u.updated_at,
-                           c.companyId,
-                           array_agg(w.warehouse) AS warehouses
-                    FROM company_user u
-                    LEFT JOIN company_user_connection c ON u.id = c.userId
-                    LEFT JOIN company_user_warehouse w ON u.id = w.userId
-                    WHERE u.email = ?
-                    GROUP BY u.id, c.companyId
-                    """)
-                .set(email)
-                .select((rset, stmt) -> {
-                    if (rset.next()) {
-                        Email userEmail = new Email(rset.getString("email"));
-                        UserName userName = new UserName(
-                                rset.getString("firstName"),
-                                rset.getString("secondName"),
-                                rset.getString("lastName")
-                        );
-                        Phone userPhone = new Phone(rset.getString("phone"));
-                        List<UserPermission> userPermissions = Arrays.stream(
-                                (String[]) rset.getArray("privileges").getArray()
-                        ).map(UserPermission::valueOf).toList();
-
-                        UUID companyId = rset.getObject("companyId", UUID.class);
-
-                        // Получение списка warehouses
-                        List<UUID> warehouses = Arrays.stream(
-                                (UUID[]) rset.getArray("warehouses").getArray()
-                        ).filter(Objects::nonNull).toList();
-
-                        return Optional.of(new User(
-                                UUID.fromString(rset.getString("id")),
-                                userName,
-                                userEmail,
-                                userPhone,
-                                rset.getString("password"),
-                                companyId,
-                                userPermissions,
-                                rset.getDate("created_at").toLocalDate(),
-                                rset.getDate("updated_at").toLocalDate(),
-                                warehouses
-                        ));
-                    } else {
-                        return Optional.empty();
-                    }
-                });
-    }
-
-
-    public Optional<List<UserPermission>> getPermissions(UUID userId) throws SQLException {
-        JdbcSession jdbcSession = new JdbcSession(dataSource);
-        return jdbcSession
-                .sql("""
-                        SELECT privileges FROM users
-                        WHERE id = ?
-                        """)
-                .set(userId)
-                .select((rset, stmt) -> {
-                            if (rset.next()) {
-                                return Optional.of(Arrays.stream(
-                                        (String[]) rset.getArray("privileges").getArray()
-                                ).map(UserPermission::valueOf).toList());
-                            } else {
-                                return Optional.empty();
-                            }
-                        }
-                );
-    }
-
-    public Optional<List<UUID>> getWarehouses(UUID userId) throws SQLException {
-        JdbcSession jdbcSession = new JdbcSession(dataSource);
-        return jdbcSession
-                .sql("""
-                        SELECT warehouses FROM users
-                        WHERE id = ?
-                        """)
-                .set(userId)
-                .select((rset, stmt) -> {
-                            if (rset.next()) {
-                                return Optional.of(Arrays.stream(
-                                        (String[]) rset.getArray("warehouses").getArray()
-                                ).map(UUID::fromString).toList());
-                            } else {
-                                return Optional.empty();
-                            }
-                        }
-                );
-    }
 
     public Optional<User> add(UserRequestEntity user) throws SQLException {
         JdbcSession jdbcSession = new JdbcSession(dataSource);
@@ -158,4 +58,69 @@ public class RpUser {
                 )
         );
     }
+
+    public Optional<User> getByEmail(String email) throws SQLException {
+        JdbcSession jdbcSession = new JdbcSession(dataSource);
+        return jdbcSession
+                .sql("""
+                    SELECT u.id,
+                           (u.name).first_name as firstName,
+                           (u.name).second_name as secondName,
+                           (u.name).last_name as lastName,
+                           u.password,
+                           u.privileges,
+                           u.email,
+                           u.phone,
+                           u.created_at,
+                           u.updated_at,
+                           c.companyId,
+                           array_agg(w.warehouse) AS warehouses
+                    FROM company_user u
+                    LEFT JOIN company_user_connection c ON u.id = c.userId
+                    LEFT JOIN company_user_warehouse w ON u.id = w.userId
+                    WHERE u.email = ?
+                    GROUP BY u.id, c.companyId
+                    """)
+                .set(email)
+                .select((rset, stmt) -> {
+                    if (rset.next()) {
+                        return compactUserFromResultSet(rset);
+                    } else {
+                        return Optional.empty();
+                    }
+                });
+    }
+
+    private static Optional<User> compactUserFromResultSet(ResultSet rset) throws SQLException {
+        Email userEmail = new Email(rset.getString("email"));
+        UserName userName = new UserName(
+                rset.getString("firstName"),
+                rset.getString("secondName"),
+                rset.getString("lastName")
+        );
+        Phone userPhone = new Phone(rset.getString("phone"));
+        List<UserPermission> userPermissions = Arrays.stream(
+                (String[]) rset.getArray("privileges").getArray()
+        ).map(UserPermission::valueOf).toList();
+
+        UUID companyId = rset.getObject("companyId", UUID.class);
+
+        List<UUID> warehouses = Arrays.stream(
+                (UUID[]) rset.getArray("warehouses").getArray()
+        ).filter(Objects::nonNull).toList();
+
+        return Optional.of(new User(
+                UUID.fromString(rset.getString("id")),
+                userName,
+                userEmail,
+                userPhone,
+                rset.getString("password"),
+                companyId,
+                userPermissions,
+                rset.getDate("created_at").toLocalDate(),
+                rset.getDate("updated_at").toLocalDate(),
+                warehouses
+        ));
+    }
+
 }
