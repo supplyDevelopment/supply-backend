b51e1c3e0b5dd16750714a2f11964fc45887e9b6
GG_Host <gd.host@yandex.ru>
2024-11-13 20:50:53 +0300

    feat: issue creation (11.1)

--- src/main/java/ru/supply/data/user/RpUser.java
+++ src/main/java/ru/supply/data/user/RpUser.java
@@ -58,7 +58,7 @@ public class RpUser {
                 )
         );
     }
-
+    // @TODO create connection pool10
     public Optional<User> getByEmail(String email) throws SQLException {
         JdbcSession jdbcSession = new JdbcSession(dataSource);
         return jdbcSession
