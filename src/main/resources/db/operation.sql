3861a9c0b9ea734d2c8a2a681a4674e283fb20f3
GG_Host <gd.host@yandex.ru>
2024-12-16 23:28:01 +0300

    feat: sql scripts finalized

---
+++ src/main/resources/db/operation.sql
@@ -0,0 +1,22 @@
+CREATE TYPE OPERATION_TYPE AS ENUM (
+    'ADD',
+    'REMOVE',
+    'REPLACE'
+)
+
+CREATE TABLE operation(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    type OPERATION_TYPE NOT NULL,
+    quantity INT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE comment(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    text VARCHAR(1024) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+-- TODO: create connection to warehouse
+-- TODO: create connection to user
+-- TODO: create connection to comment
