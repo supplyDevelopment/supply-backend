7d18c02f0ac3e8fb07b1a2d2aec594a19375d64a
GG_Host <gd.host@yandex.ru>
2025-01-09 00:31:07 +0300

    feat: company methods to get information connected to company implemented

--- src/main/java/supply/server/data/company/Company.java
+++ src/main/java/supply/server/data/company/Company.java
@@ -1,20 +1,21 @@
 package supply.server.data.company;
 
+import com.jcabi.jdbc.JdbcSession;
 import org.apache.commons.lang3.NotImplementedException;
-import supply.server.data.project.Project;
-import supply.server.data.resource.Resource;
+import supply.server.data.PaginatedList;
+import supply.server.data.Pagination;
 import supply.server.data.supplier.Supplier;
-import supply.server.data.user.User;
 import supply.server.data.utils.Address;
-import supply.server.data.utils.company.Bil;
 import supply.server.data.utils.Email;
 import supply.server.data.utils.Phone;
+import supply.server.data.utils.company.Bil;
 import supply.server.data.utils.company.CompanyStatus;
 import supply.server.data.utils.company.Tax;
-import supply.server.data.warehouse.Warehouse;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -32,23 +33,123 @@ public record Company (
         DataSource dataSource
 ) {
 
-    public List<Project> projects() {
-        throw new NotImplementedException();
+    public PaginatedList<UUID> projectIds(Pagination pagination) throws SQLException {
+        JdbcSession jdbcSession = new JdbcSession(dataSource);
+
+        return jdbcSession
+                .sql("""
+                        SELECT
+                            cp.project,
+                            COUNT(*) OVER() AS total_count
+                        FROM company_projects cp
+                        WHERE cp.company = ?
+                        LIMIT ?
+                        OFFSET ?
+                        """)
+                .set(id())
+                .set(pagination.limit())
+                .set(pagination.offset())
+                .select((rset, stmt) -> {
+                    List<UUID> ids = new ArrayList<>();
+                    long total = 0;
+                    while (rset.next()) {
+                        ids.add(rset.getObject("project", UUID.class));
+                        if (total == 0) {
+                            total = rset.getLong("total_count");
+                        }
+                    }
+                    return new PaginatedList<>(total, ids);
+                });
     }
 
-    public List<Warehouse> warehouses() {
-        throw new NotImplementedException();
+    public PaginatedList<UUID> warehouseIds(Pagination pagination) throws SQLException {
+        JdbcSession jdbcSession = new JdbcSession(dataSource);
+
+        return jdbcSession
+                .sql("""
+                        SELECT
+                            cw.warehouse,
+                            COUNT(*) OVER() AS total_count
+                        FROM company_warehouses cw
+                        WHERE cw.company = ?
+                        LIMIT ?
+                        OFFSET ?
+                        """)
+                .set(id())
+                .set(pagination.limit())
+                .set(pagination.offset())
+                .select((rset, stmt) -> {
+                    List<UUID> ids = new ArrayList<>();
+                    long total = 0;
+                    while (rset.next()) {
+                        ids.add(rset.getObject("warehouse", UUID.class));
+                        if (total == 0) {
+                            total = rset.getLong("total_count");
+                        }
+                    }
+                    return new PaginatedList<>(total, ids);
+                });
     }
 
-    public List<User> users() {
-        throw new NotImplementedException();
+    public PaginatedList<UUID> userIds(Pagination pagination) throws SQLException {
+        JdbcSession jdbcSession = new JdbcSession(dataSource);
+
+        return jdbcSession
+                .sql("""
+                        SELECT
+                            cu.user_id,
+                            COUNT(*) OVER() AS total_count
+                        FROM company_users cu
+                        WHERE cu.company_id = ?
+                        LIMIT ?
+                        OFFSET ?
+                        """)
+                .set(id())
+                .set(pagination.limit())
+                .set(pagination.offset())
+                .select((rset, stmt) -> {
+                    List<UUID> ids = new ArrayList<>();
+                    long total = 0;
+                    while (rset.next()) {
+                        ids.add(rset.getObject("user_id", UUID.class));
+                        if (total == 0) {
+                            total = rset.getLong("total_count");
+                        }
+                    }
+                    return new PaginatedList<>(total, ids);
+                });
     }
 
-    public List<Resource> resources() {
-        throw new NotImplementedException();
+    public PaginatedList<UUID> resourceIds(Pagination pagination) throws SQLException {
+        JdbcSession jdbcSession = new JdbcSession(dataSource);
+
+        return jdbcSession
+                .sql("""
+                        SELECT
+                            cr.resource,
+                            COUNT(*) OVER() AS total_count
+                        FROM company_resources cr
+                        WHERE cr.company = ?
+                        LIMIT ?
+                        OFFSET ?
+                        """)
+                .set(id())
+                .set(pagination.limit())
+                .set(pagination.offset())
+                .select((rset, stmt) -> {
+                    List<UUID> ids = new ArrayList<>();
+                    long total = 0;
+                    while (rset.next()) {
+                        ids.add(rset.getObject("resource", UUID.class));
+                        if (total == 0) {
+                            total = rset.getLong("total_count");
+                        }
+                    }
+                    return new PaginatedList<>(total, ids);
+                });
     }
 
-    public List<Supplier> suppliers() {
+    public List<Supplier> supplierIds() {
         throw new NotImplementedException();
     }
 
