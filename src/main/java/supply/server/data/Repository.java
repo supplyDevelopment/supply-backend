7cde218422694e021c6461bbcbec1c84cc782a82
GG_Host <gd.host@yandex.ru>
2025-01-09 00:50:14 +0300

    feat: repository constructor updated

--- src/main/java/supply/server/data/Repository.java
+++ src/main/java/supply/server/data/Repository.java
@@ -6,6 +6,8 @@ import supply.server.data.resource.RpResource;
 import supply.server.data.user.RpUser;
 import supply.server.data.warehouse.RpWarehouse;
 
+import javax.sql.DataSource;
+
 public record Repository(
         RpCompany rpCompany,
         RpUser rpUser,
@@ -13,4 +15,13 @@ public record Repository(
         RpProject rpProject,
         RpResource rpResource
 ) {
+    public Repository(DataSource dataSource) {
+        this(
+                new RpCompany(dataSource),
+                new RpUser(dataSource),
+                new RpWarehouse(dataSource),
+                new RpProject(dataSource),
+                new RpResource(dataSource)
+        );
+    }
 }
