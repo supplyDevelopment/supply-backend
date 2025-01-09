2c3a8a766bbdb5a4f48ec14d05acc97219c0e1c6
GG_Host <gd.host@yandex.ru>
2025-01-09 19:56:56 +0300

    feat: CompanyService updated

--- src/main/java/supply/server/service/CompanyService.java
+++ src/main/java/supply/server/service/CompanyService.java
@@ -2,6 +2,7 @@ package supply.server.service;
 
 import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.NotImplementedException;
+import supply.server.configuration.exception.DbException;
 import supply.server.data.PaginatedList;
 import supply.server.data.Pagination;
 import supply.server.data.PgRepository;
@@ -22,51 +23,72 @@ import java.util.UUID;
 public class CompanyService {
 
     private final Company company;
-    private final PgRepository pgRepository;
+    private final RepositoryService repository;
 
-    public PaginatedList<Project> projects(Pagination pagination) throws SQLException {
-        PaginatedList<UUID> projectIds = company.projectIds(pagination);
+    public PaginatedList<Project> projects(Pagination pagination) {
+        PaginatedList<UUID> projectIds;
+        try {
+            projectIds = company.projectIds(pagination);
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
 
         List<Project> projects = new ArrayList<>();
         for (UUID id: projectIds.items()) {
-            Optional<Project> project = pgRepository.rpProject().get(id);
-            project.ifPresent(projects::add);
+            Project project = repository.getProject().get(id);
+            projects.add(project);
         }
 
         return new PaginatedList<>(projectIds.total(), projects);
     }
 
-    public PaginatedList<Warehouse> warehouses(Pagination pagination) throws SQLException {
-        PaginatedList<UUID> warehouseIds = company.warehouseIds(pagination);
+    public PaginatedList<Warehouse> warehouses(Pagination pagination) {
+        PaginatedList<UUID> warehouseIds;
+        try {
+            warehouseIds = company.warehouseIds(pagination);
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
 
         List<Warehouse> warehouses = new ArrayList<>();
         for (UUID id: warehouseIds.items()) {
-            Optional<Warehouse> warehouse = pgRepository.rpWarehouse().getById(id);
-            warehouse.ifPresent(warehouses::add);
+            Warehouse warehouse = repository.getWarehouse().get(id);
+            warehouses.add(warehouse);
         }
 
         return new PaginatedList<>(warehouseIds.total(), warehouses);
     }
 
-    public PaginatedList<User> users(Pagination pagination) throws SQLException {
-        PaginatedList<UUID> userIds = company.userIds(pagination);
+    public PaginatedList<User> users(Pagination pagination) {
+        PaginatedList<UUID> userIds;
+        try {
+            userIds = company.userIds(pagination);
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
 
         List<User> users = new ArrayList<>();
         for (UUID id: userIds.items()) {
-            Optional<User> user = pgRepository.rpUser().get(id);
-            user.ifPresent(users::add);
+            User user = repository.getUser().get(id);
+            users.add(user);
         }
 
         return new PaginatedList<>(userIds.total(), users);
     }
 
-    public PaginatedList<Resource> resources(Pagination pagination) throws SQLException {
-        PaginatedList<UUID> resourceIds = company.resourceIds(pagination);
+    public PaginatedList<Resource> resources(Pagination pagination) {
+        PaginatedList<UUID> resourceIds;
+
+        try {
+            resourceIds = company.resourceIds(pagination);
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
 
         List<Resource> resources = new ArrayList<>();
         for (UUID id: resourceIds.items()) {
-            Optional<Resource> resource = pgRepository.rpResource().get(id);
-            resource.ifPresent(resources::add);
+            Resource resource = repository.getResource().get(id);
+            resources.add(resource);
         }
 
         return new PaginatedList<>(resourceIds.total(), resources);
