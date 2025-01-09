8259686ce27480ed107baa3bd53de0ea1bb9e366
GG_Host <gd.host@yandex.ru>
2025-01-09 19:56:41 +0300

    feat: RepositoryService implemented

--- src/main/java/supply/server/service/RepositoryService.java
+++ src/main/java/supply/server/service/RepositoryService.java
@@ -1,30 +1,47 @@
 package supply.server.service;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import supply.server.data.InMemoryRepository;
 import supply.server.data.PgRepository;
-import supply.server.service.repository.CompanyRepositoryService;
-import supply.server.service.repository.UserRepositoryService;
+import supply.server.service.repository.*;
 
 public class RepositoryService {
 
     private final PgRepository pgRepository;
     private final InMemoryRepository inMemoryRepository;
 
-    private final CompanyRepositoryService companyRepositoryService;
-    private final UserRepositoryService userRepositoryService;
+    @Getter
+    private final CompanyRepositoryService company;
+    @Getter
+    private final UserRepositoryService user;
+    @Getter
+    private final ProjectRepositoryService project;
+    @Getter
+    private final WarehouseRepositoryService warehouse;
+    @Getter
+    private final ResourceRepositoryService resource;
 
     public RepositoryService(PgRepository pgRepository, InMemoryRepository inMemoryRepository) {
         this.pgRepository = pgRepository;
         this.inMemoryRepository = inMemoryRepository;
 
-        companyRepositoryService = new CompanyRepositoryService(
+        company = new CompanyRepositoryService(
                 pgRepository.rpCompany(),
                 inMemoryRepository.inMemoryRpCompany()
         );
-        userRepositoryService = new UserRepositoryService(
+        user = new UserRepositoryService(
                 pgRepository.rpUser(),
                 inMemoryRepository.inMemoryRpUser()
+        );
+        project = new ProjectRepositoryService(
+                pgRepository.rpProject()
+        );
+        warehouse = new WarehouseRepositoryService(
+                pgRepository.rpWarehouse()
+        );
+        resource = new ResourceRepositoryService(
+                pgRepository.rpResource()
         );
     }
 
