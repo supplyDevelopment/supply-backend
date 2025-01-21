7452ba63840db3fb70ef7185d443ba6fb6c90a9e
GG_Host <gd.host@yandex.ru>
2025-01-21 21:25:50 +0300

    feat: prototype implemented

---
+++ dump-service/src/main/java/supply/server/config/DatabaseConfig.java
@@ -0,0 +1,11 @@
+package supply.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfig {
+
+    // TODO: implement bean for connection with cloud
+
+}
