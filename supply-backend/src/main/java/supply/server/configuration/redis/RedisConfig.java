82be3ba3d4eca26194086beaed56b8e26731f2ec
GG_Host <gd.host@yandex.ru>
2025-01-22 22:46:10 +0300

    feat: configuration updated feat: docker-compose file updated to run redis

--- supply-backend/src/main/java/supply/server/configuration/redis/RedisConfig.java
+++ supply-backend/src/main/java/supply/server/configuration/redis/RedisConfig.java
@@ -1,8 +1,10 @@
 package supply.server.configuration.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -13,9 +15,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration("localhost", 6379);
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration("redis", 6379);
+        redisConfig.setPassword(RedisPassword.of(redisPassword));
 
         LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
                 .commandTimeout(java.time.Duration.ofSeconds(60))
