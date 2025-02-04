75547a22191ac5e3f6d4b425bd8934404cbe6169
GG_Host <gd.host@yandex.ru>
2025-02-04 18:20:57 +0300

    feat: AuthorisationController and SubscriptionController implemented

---
+++ supply-backend/src/main/java/supply/server/controller/end_point/AuthorisationController.java
@@ -0,0 +1,53 @@
+package supply.server.controller.end_point;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import supply.server.controller.entity.request.EmailPasswordRequest;
+import supply.server.controller.entity.request.EmailRequest;
+import supply.server.service.dataService.AuthenticationService;
+import supply.server.service.dataService.CompanyService;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+@Tag(name = "Authorisation controller" , description = "Registration and login")
+public class AuthorisationController {
+
+    private final CompanyService companyService;
+    private final AuthenticationService authenticationService;
+
+
+    @PostMapping("/register_company")
+    public ResponseEntity<?> registerCompany(@RequestBody @Valid @NotNull EmailRequest email, HttpServletResponse response) {
+        String jwt = companyService.createCompany(email.toEmail());
+        authenticationService.setAuthenticationCookie(response, jwt);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/authorize")
+    public ResponseEntity<?> authorize(@RequestBody @Valid @NotNull EmailPasswordRequest emailPassword, HttpServletResponse response) {
+        Optional<String> jwtOptional = authenticationService.authenticate(emailPassword.toEmail(), emailPassword.password());
+        if (jwtOptional.isPresent()) {
+            authenticationService.setAuthenticationCookie(response, jwtOptional.get());
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
+        authenticationService.resetAuthenticationCookie(request, response);
+        return ResponseEntity.ok().build();
+    }
+
+}
