2fefd32fb0c3d8ea48cab987ea57a9f4e8038e14
GG_Host <gd.host@yandex.ru>
2025-02-06 18:21:33 +0300

    feat: implemented methods for controllers

--- supply-backend/src/main/java/supply/server/controller/end_point/StackController.java
+++ supply-backend/src/main/java/supply/server/controller/end_point/StackController.java
@@ -1,15 +1,22 @@
 package supply.server.controller.end_point;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import supply.server.controller.entity.request.CreateResourceRequest;
+import supply.server.controller.entity.request.EditResourceRequest;
 import supply.server.controller.entity.request.PaginationRequest;
 import supply.server.data.PaginatedList;
 import supply.server.data.resource.Resource;
 import supply.server.service.dataService.CreationService;
+import supply.server.service.dataService.FetchService;
 import supply.server.service.dataService.SearchService;
+import supply.server.service.dataService.UpdateService;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/stack")
@@ -19,18 +26,38 @@ public class StackController {
 
     private final CreationService creationService;
     private final SearchService searchService;
+    private final UpdateService updateService;
+    private final FetchService fetchService;
 
     @PostMapping("/add")
-    public ResponseEntity<?> addStack(@RequestBody CreateResourceRequest createResource) {
+    public ResponseEntity<?> addStack(@RequestBody @Valid @NotNull CreateResourceRequest createResource) {
         creationService.createResource(createResource.toCreateResource());
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/stacks")
-    public ResponseEntity<?> getStacks(@RequestParam String prefix, @RequestParam PaginationRequest paginationRequest) {
+    public ResponseEntity<?> getStacks(@RequestParam @Valid @NotNull String prefix, @Valid @NotNull PaginationRequest paginationRequest) {
         PaginatedList<Resource> resources = searchService.getResources(prefix, paginationRequest.toPagination());
 
         return ResponseEntity.ok(resources);
+    }
+
+    @GetMapping("/stacks/{id}")
+    public ResponseEntity<?> getStack(@PathVariable String id) {
+        Resource resource = fetchService.getResource(UUID.fromString(id));
+        return ResponseEntity.ok(resource);
+    }
+
+    @PostMapping("/expand")
+    public ResponseEntity<?> expandStack(@RequestBody @Valid @NotNull String id, @Valid @NotNull Integer quantity) {
+        updateService.expendResource(UUID.fromString(id), quantity);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/edit")
+    public ResponseEntity<?> editStack(@RequestBody @Valid @NotNull EditResourceRequest editResourceRequest, @Valid @NotNull String id) {
+        updateService.updateResource(UUID.fromString(id), editResourceRequest.toEditResource());
+        return ResponseEntity.ok().build();
     }
 
 }
