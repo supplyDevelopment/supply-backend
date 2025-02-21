b61d33f6853cc9f74f0656a7228c2fc24d116ea8
GG_Host <gd.host@yandex.ru>
2025-02-21 23:19:31 +0300

    feat: error log updated

--- supply-backend/src/main/java/supply/server/service/ExceptionHandlerService.java
+++ supply-backend/src/main/java/supply/server/service/ExceptionHandlerService.java
@@ -7,55 +7,57 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import supply.server.configuration.exception.*;
 
+import java.time.LocalTime;
+
 @Slf4j
 @ControllerAdvice
 public class ExceptionHandlerService {
 
     @ExceptionHandler({RedisLockException.class, DbException.class})
     public ResponseEntity<?> handleExceptionServer(Exception e) {
-        log.error("Storage error: {}\n {}", e.getMessage(), e.getStackTrace());
+        log.error("{} Storage error: {}", LocalTime.now(), e.getMessage());
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 
     @ExceptionHandler({DataNotFoundException.class})
     public ResponseEntity<?> handleExceptionBadRequest(DataNotFoundException e) {
-        log.warn("Data not found: {}", e.getMessage());
+        log.warn("{} Data not found: {}", LocalTime.now(), e.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(IncorrectInputException.class)
     public ResponseEntity<?> handleExceptionBadRequest(IncorrectInputException e) {
-        log.warn("Incorrect input: {}", e.getMessage());
+        log.warn("{} Incorrect input: {}", LocalTime.now(), e.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(IncorrectParameterException.class)
     public ResponseEntity<?> handleExceptionBadRequest(IncorrectParameterException e) {
-        log.warn("Incorrect parameter: {}", e.getMessage());
+        log.warn("{} Incorrect parameter: {}", LocalTime.now(), e.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<?> handleExceptionUnauthorized(AuthenticationException e) {
-        log.warn("Authorization error: {}", e.getMessage());
+        log.warn("{} Authorization error: {}", LocalTime.now(), e.getMessage());
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 
     @ExceptionHandler(PaymentException.class)
     public ResponseEntity<?> handleExceptionPayment(PaymentException e) {
-        log.warn("Payment error: {}", e.getMessage());
+        log.warn("{} Payment error: {}", LocalTime.now(), e.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(Exception e) {
-        log.error("Server error: {}\n {}", e.getMessage(), e.getStackTrace());
+        log.error("{} Server error: {}", LocalTime.now(), e.getMessage());
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
